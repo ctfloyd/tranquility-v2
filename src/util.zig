4906ee56b691c8f4a7828f772d0e4928419175e8
ctfloyd <calebfloyd0899@gmail.com>
2024-03-23 20:08:03 -0500

    util methods to concat strings and format number as strings

--- src/util.zig
+++ src/util.zig
@@ -5,3 +5,22 @@ pub fn own_str(allocator: std.mem.Allocator, str: []const u8) ![]const u8 {
     @memcpy(my_str, str);
     return my_str;
 }
+
+pub fn concat(allocator: std.mem.Allocator, comptime num_args: comptime_int, args: anytype) []const u8 {
+    return std.fmt.allocPrint(allocator, "{s}" ** num_args, args) catch @panic("OOM");
+}
+
+pub fn number_to_string(allocator: std.mem.Allocator, value: anytype) []const u8 {
+    return switch (@typeInfo(@TypeOf(value))) {
+        .Int, .Float => number_to_string_unsafe(allocator, value),
+        .ComptimeInt, .ComptimeFloat => |v| {
+            const coerced_value: i128 = v;
+            return number_to_string_unsafe(allocator, coerced_value);
+        },
+        else => unreachable,
+    };
+}
+
+fn number_to_string_unsafe(allocator: std.mem.Allocator, value: anytype) []const u8 {
+    return std.fmt.allocPrint(allocator, "{d}", .{value}) catch @panic("OOM");
+}
